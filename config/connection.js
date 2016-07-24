7b08059bb09a9527360830b42b1d4f5ebb977577
James Hornung <hornung.james@gmail.com>
2016-07-24 16:16:43 -0500

    connection.js and orm.js pages updated

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,21 @@
-//Create a connection.js file inside config directory.
-// Inside the connection.js file, setup the code to connect Node to MySQL.
-// Export the connection.
+//Connet to mysql and export the connection
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'burgers_db'
+});
+
+
+connection.connect(function (err) {
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+	console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,28 @@
 // Import (require) connection.js into orm.js
-
 // In the orm.js file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
-
 // selectAll()
 // insertOne()
 // updateOne()
 // Export the ORM object in module.exports.
+
+var connection = require('./connection.js');
+
+var orm = {
+	selectAll: function(){
+		var queryString = 'SELECT * FROM burgers';
+		connection.query(queryString, function(err, result){
+			console.log(result);
+		})
+	},
+	insertOne: function(table, burgerName, beenDevoured){
+		var queryString = 'INSERT INTO ' + table +  ' (burger_name, devoured) VALUES (' + burgerName + ', ' + beenDevoured + ')';
+		connection.query(queryString, function(err, result){
+			console.log(result);
+		})
+	},
+	updateOne: function(){
+		console.log('need to update one');
+	}
+};
+
+module.exports = orm;
