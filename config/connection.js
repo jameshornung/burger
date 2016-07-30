cc1f2fa6a9a9b39b9658ce20ac8b0b693c35b4cd
James Hornung <hornung.james@gmail.com>
2016-07-30 12:20:32 -0500

    current version

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,23 @@
 
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'burgers_db'
-});
+var source = {
+	localhost: {
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'burgers_db'
+	},
+	jaws: {
+		port: 3306,
+        host: 'Mysql@l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306',
+        user: 'ewuqx1n5w8jo6x4v',
+        password: "yuhl98jsef0k7ul4",
+        database: "lghowl35ljxl5vao" 
+	}
+};
 
+var connection = mysql.createConnection(source.jawsDB);
 
 connection.connect(function (err) {
 	if (err) {
